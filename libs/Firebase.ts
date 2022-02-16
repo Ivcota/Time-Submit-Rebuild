2ccb578266758f4a4be092707ca5a59c1b3193c3
Ivcota <ivcotad@gmail.com>
2022-02-16 10:23:51 -0700

    firebase init

---
+++ libs/Firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API,
+  authDomain: process.env.NEXT_PUBLIC_AUTH,
+  projectId: "time-collection-app",
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE,
+  messagingSenderId: "484136309667",
+  appId: "1:484136309667:web:a372abfbeacbaa8fa40016",
+  measurementId: "G-51FXCCH62J",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
